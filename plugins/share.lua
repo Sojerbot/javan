3495e2f6b1a389c40e76644f100ba883c3526f72
mohammad <Sojerbot@users.noreply.github.com>
2016-05-13 01:54:05 +0430

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -2,14 +2,14 @@ do
 
 function run(msg, matches)
 
-if matches[1] == 'share' then
-send_contact(get_receiver(msg), "+6288973997158", "ѕвѕѕ", "Use /Share To Get Number", ok_cb, false)
+if matches[1] == 'شماره سازندت' then
+send_contact(get_receiver(msg), "+989331245256", "mohammad(sudo)", "", ok_cb, false)
 end
 end
 return {
 patterns = {
-"^[#!/]([Ss][Hh][Aa][Rr][Ee])$",
-"^([Ss][Hh][Aa][Rr][Ee])$"
+"^شماره سازندت$",
+"^شماره سازندت$"
 
 },
 run = run
